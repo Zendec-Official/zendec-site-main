6b4f674941cc5186a931f801e011b04513ce7788
Mohammed Shimaz <mohammedshimaz87@gmail.com>
2025-10-25 12:12:33 +0530

    Updated hero section to show some test

---
+++ src/components/Hero.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div className="h-screen flex items-center justify-center bg-gray-100">
+        Hero
+    </div>
+  )
+}
+
+export default Hero
